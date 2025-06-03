ad71f78ecca8cc5824127e0dcc95637c9651bd40
Phyrone <phyrone@phyrone.de>
2025-06-03 11:22:45 +0200

    feat: enhance AppDock and AppSkeleton components with search functionality and code cleanup

--- app/src/routes/(app)/AppDock.svelte
+++ app/src/routes/(app)/AppDock.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
-    import {HomeIcon, UserIcon} from '@lucide/svelte';
-    import type {PageId} from "$lib/page";
-    import {page} from '$app/state'
+	import { HomeIcon, UserIcon,SearchIcon } from '@lucide/svelte';
+	import { page } from '$app/state';
 
 
-    type DockGroup = 'home' | 'profile';
-    let dock_group = $derived.by(() => {
-        if (page.route.id?.startsWith('/(app)/self')) {
-            return 'profile';
-        } else {
-            return 'home';
-        }
-    })
+	type DockGroup = 'home' | 'profile';
+	let dock_group: DockGroup | undefined = $derived.by(() => {
+		if (page.route.id?.startsWith('/(app)/self')) {
+			return 'profile';
+		} else {
+			return 'home';
+		}
+	});
 
 </script>
 
 <div class="dock sm:hidden ">
-    <a class={{
+	<a class={{
         'dock-active': dock_group === 'home'
     }} href="/">
-        <HomeIcon/>
-        Home
-    </a>
-    <a class={{
+		<HomeIcon />
+		Home
+	</a>
+	<button class="btn btn-xl btn-primary btn-circle flex-none ">
+		<SearchIcon />
+	</button>
+	<a class={{
         'dock-active': dock_group === 'profile'
     }} href="/self">
-        <UserIcon/>
-        Profile
-    </a>
+		<UserIcon />
+		Profile
+	</a>
 </div>
--- app/src/routes/(app)/AppSkeleton.svelte
+++ app/src/routes/(app)/AppSkeleton.svelte
@@ -4,8 +4,6 @@
     import AppDock from './AppDock.svelte';
     import {ready_to_update} from "$lib/sw-ctl";
     import type {ServiceWorkerInstruction} from "$lib/sw-msg";
-    import {page} from '$app/state'
-    import {get_page_id} from "$lib/page";
 
     type Props = {
         children?: Snippet;
@@ -17,13 +15,9 @@
         $ready_to_update?.postMessage({type: 'SKIP_WAITING'} satisfies ServiceWorkerInstruction)
     }
 
-    let page_id = $derived(get_page_id(page?.route?.id))
-
 </script>
 
-{page_id}
 <AppNavbar/>
-
 {#if $ready_to_update}
     <button class="btn" onclick={triggerUpdate}>Update</button>
 {/if}
